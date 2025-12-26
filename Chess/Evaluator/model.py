15c4240a70eee719c4b23581b0b657763fadf801
Tala105 <marcuscorboo@gmail.com>
2025-12-26 20:04:12 -0300

    Base Evaluator model defined

--- Chess/Evaluator/model.py
+++ Chess/Evaluator/model.py
@@ -1,5 +1,31 @@
 import tensorflow as tf
-import numpy as np
-import pandas as pd
+import keras
+from collections import deque
 
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+class Evaluator:
+    def __init__(self, state_size=67, gamma=0.95, epsilon=1.0, epsilon_min=0.05,
+                 epsilon_decay=0.99, learning_rate=0.001, buffer_size=10000):
+        self.state_size = state_size
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.epsilon_min = epsilon_min
+        self.epsilon_decay = epsilon_decay
+        self.learning_rate = learning_rate
+        self.replay_buffer = deque(maxlen=buffer_size)
+        
+        self._build_model()
+    
+    def _build_model(self):
+        self.model = keras.Sequential([
+            keras.layers.Input(shape=(self.state_size,0), dtype=("int32")),
+        ])
+        for _ in range(8):
+            self.model.add(keras.layers.Dense(128))
+            self.model.add(keras.layers.LeakyReLU(alpha=0.01))
+            self.model.add(keras.layers.BatchNormalization())
+            self.model.add(keras.layers.Dropout(0.2))
+        self.model.add(keras.layers.Dense(1))
+
+        self.model.compile(optimizer="adam", loss="mse", metrics=["mse"])
+
+eva = Evaluator()
